0c2f89ca78f9e184260b0cc1a8101c4a19ff4884
Arima Kaoru <46063788+arimakaoru@users.noreply.github.com>
2020-08-20 12:22:50 +0900

    Update: Forクラスのテストコードを追加

--- src/test/java/io/github/morichan/retuss/language/java/ForTest.java
+++ src/test/java/io/github/morichan/retuss/language/java/ForTest.java
@@ -8,17 +8,53 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class ForTest {
     For forClass;
+    LocalVariableDeclaration lvd;
 
     @Nested
     class 正しい使い方の場合 {
         @BeforeEach
-        void setup() { forClass = new For("int i=0", "i<10", "i++"); }
+        void setup() {
+            forClass = new For("int i=0", "i<10", "i++");
+            lvd = new LocalVariableDeclaration(new Type("int"), "var");
+        }
+
+        @Test
+        void 初期化式を返す() {
+            String expected = "int i=0";
+            assertThat(forClass.getForInit()).isEqualTo((expected));
+        }
+
+        @Test
+        void 継続条件式を返す() {
+            String expected = "i<10";
+            assertThat(forClass.getExpression()).isEqualTo((expected));
+        }
+
+        @Test
+        void 更新式を返す() {
+            String expected = "i++";
+            assertThat(forClass.getForUpdate()).isEqualTo((expected));
+        }
 
         @Test
         void for文を返す() {
             String expected = "for (int i=0; i<10; i++) {\n        }\n";
             assertThat(forClass.getStatement()).isEqualTo(expected);
         }
+
+        @Test
+        void numLoopの設定と取得() {
+            String numLoop = "10";
+            forClass.setNumLoop((numLoop));
+            assertThat(forClass.getNumLoop()).isEqualTo(numLoop);
+        }
+
+        @Test
+        void statementの追加と取得() {
+            forClass.addStatement(lvd);
+            assertThat(forClass.getStatements().get(0)).isEqualTo(lvd);
+        }
+
     }
 
     @Nested
